02a3f8031c4254d7fab53145fab541102dde9565
CANO <umutcanbozyigit34012@gmail.com>
2023-12-06 00:07:08 +0300

    static

---
+++ src/Alan.java
@@ -0,0 +1,14 @@
+public class Alan {
+    public int r;
+    public final double pi=3.14;
+
+    public Alan(int r){
+        this.r=r;
+
+    }
+    public void alanhesapla(){
+        double alan= pi*this.r*this.r;
+        System.out.println("dairenin alanı: "+alan);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Alan aln=new Alan(10);
+        aln.alanhesapla();
+
+    }
+}
